674766cc470e1795f5ec74042abe704e41ac560c
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-08-13 21:59:52 +0000

    chore: rename IconsRow icon

--- src/blocks/IconsRow/index.ts
+++ src/blocks/IconsRow/index.ts
@@ -1,14 +1,20 @@
 import { Block } from 'payload/types';
 import * as fa6Icons from 'react-icons/fa6';
 import iconPickerField from '@innovixx/payload-icon-picker-field';
+import { RowLabelArgs } from 'payload/dist/admin/components/forms/RowLabel/types';
 
 export const IconsRow: Block = {
   fields: [
     {
+      admin: {
+        components: {
+          RowLabel: ({ data }: RowLabelArgs) => `Icon - ${data.icon}`,
+        },
+      },
       fields: [
         iconPickerField({
-          label: 'React Icons',
-          name: 'reactIconsIcon',
+          label: 'Icons',
+          name: 'icon',
           reactIconPack: fa6Icons,
         }),
       ],
